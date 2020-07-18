a5bfe2325820c8eb3b0c9a2bb824ae148623ceb8
dlwngus1129 <56718922+dlwngus1129@users.noreply.github.com>
2020-07-18 15:57:02 +0900

    유니티 실험중

---
+++ New Unity Project (2)/Assets/script/add_jump_count.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class add_jump_count : MonoBehaviour
+{
+    public GameObject Yellow_block;
+    public static bool respawn_x = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.Find("add_jump_count_ob(Clone)")==false && respawn_x ==true)
+        {
+            Invoke("respawn", 2f);
+            respawn_x = false;
+        }
+    }
+   void respawn()
+    {
+        Instantiate(Yellow_block, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
---
+++ New Unity Project (2)/Assets/script/destroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class destroy : MonoBehaviour
+    {
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (GameObject.FindWithTag("Player"))                   // Player라는 태그의 오브젝트와 닿았을때
+            {
+                Destroy(GameObject.Find("add_jump_count_ob(Clone)"));  // 추가점프 블럭을 파괴
+                ex.jump_count--;
+                add_jump_count.respawn_x = true;
+            }
+
+        }
+    }
