940320cbedd97504aa437627ba6a3ebd718c7bc6
dlwngus1129 <56718922+dlwngus1129@users.noreply.github.com>
2020-07-18 15:32:40 +0900

    Create ex.cs

---
+++ ex.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+
+public class ex : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public int jump = 1;
+    public static int jump_count = 0;
+    public float dash_speed = 0;
+
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed*Time.deltaTime);
+            transform.localScale = new Vector3(1,1,1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed*Time.deltaTime);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (jump_count < 2)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+                jump_count++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right  * dash_speed,ForceMode2D.Impulse);
+        }
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (GameObject.FindWithTag("wall"))
+        {
+            jump_count = 0;
+        }
+    }
+
+}
+    
+
+        
+    
+
+    
+
